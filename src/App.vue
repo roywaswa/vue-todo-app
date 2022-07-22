40c58d51a0bf64dcd5fac0eee217e76f63407074
roywaswa <roywaswa.rw@gmail.com>
2022-07-23 01:04:38 +0300

    Input enabled

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import InputForm from './components/InputForm.vue';
 
 </script>
 
 <template>
-
+  <h1>TODO APP</h1>
+  <InputForm/>
 </template>
 
 <style>
---
+++ src/components/InputForm.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import { storeToRefs } from 'pinia'
+import { useTodoStore } from '../app/store'
+
+const  todos  = useTodoStore()
+const todo = ref({
+  id: `${Math.random()*100*100*100000}`,
+  title: 'Todo',
+  isDone: false
+})
+
+function addTodo() {
+  todos.addTodoToStore(todo.value)
+}
+
+</script>
+
+<template>
+  <div class="todo-input">
+    <form action="POST" class="todo-input" @submit.prevent="addTodo">
+      <label for="todo-field">Todo: </label>
+      <input v-model="todo.title" type="text" name="todo-field" id="todo-field">
+      <button type="submit" >ADD TODO</button>
+    </form>
+  </div>
+</template>
+
+<style>
+
+</style>
