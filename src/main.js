733636acc3e6d3799de9f45c6988a0229fce42a7
roywaswa <roywaswa.rw@gmail.com>
2022-07-25 23:32:07 +0300

    remove bug

--- src/main.js
+++ src/main.js
@@ -2,12 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from "pinia";
 import App from './App.vue'
 import './styles/main.css'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
-
-loadFonts()
 
 createApp(App)
-  .use(vuetify)
   .use(createPinia())
   .mount('#app')
